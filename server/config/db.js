dc87149d1ef139181d0e60816315556cda996680
Sanyam Munot <sanyammunot03@gmail.com>
2025-05-15 14:42:54 +0530

    security change

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://sanyammunot03:Shree98794@cluster0.5ky8tma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"); // No options needed for MongoDB Driver v4+
+    await mongoose.connect(MONGO_URI); // No options needed for MongoDB Driver v4+
     console.log('✅ MongoDB Connected');
   } catch (err) {
     console.error('❌ MongoDB Connection Error:', err.message);
