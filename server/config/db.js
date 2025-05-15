f41d450a4b9fac3da91b9cf6e2a70235d6c1086c
Sanyam Munot <sanyammunot03@gmail.com>
2025-05-15 14:45:00 +0530

    change

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI); // No options needed for MongoDB Driver v4+
+    await mongoose.connect(process.env.MONGO_URI); // No options needed for MongoDB Driver v4+
     console.log('✅ MongoDB Connected');
   } catch (err) {
     console.error('❌ MongoDB Connection Error:', err.message);
