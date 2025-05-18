e27b80a6f154ef749bae7cc7a2d009762fefa986
Sanyam Munot <sanyammunot03@gmail.com>
2025-05-18 15:07:11 +0530

    changes

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,8 +1,5 @@
-const User = require('../models/User');
-const Project = require('../models/Project');
-
-// AI Matching Algorithm (simplified)
 exports.matchAgencies = async (req, res) => {
+   console.log('matchAgencies called with body:', req.body);
   const { customerId } = req.body;
   try {
     const customer = await User.findById(customerId);
@@ -10,30 +7,39 @@ exports.matchAgencies = async (req, res) => {
       return res.status(400).json({ msg: 'Invalid customer' });
     }
 
+    const neededTypes = customer.needs.map(need => need.type);
+    console.log('Customer needs:', customer.needs);
+    console.log('Needed types:', neededTypes);
+
+    if (neededTypes.length === 0) {
+      return res.status(200).json([]);
+    }
+
     const agencies = await User.find({
       userType: 'agency',
-      'services.type': { $in: customer.needs.map(need => need.type) }
+      'services.type': { $in: neededTypes }
     });
+
+    console.log('Found agencies:', agencies.length);
 
     const matchedAgencies = agencies.map(agency => {
       let priorityScore = 0;
-
       customer.needs.forEach(need => {
-        const matches = agency.services.some(service => service.type === need.type);
-        if (matches) {
-          priorityScore += need.priority * 10; // weight of 10 per priority level
+        const hasService = agency.services.some(service => service.type === need.type);
+        if (hasService) {
+          priorityScore += need.priority * 10;
         }
       });
 
       const experienceBonus = agency.YearsOfExperience ? agency.YearsOfExperience * 3 : 0;
-      //const ratingBonus = agency.averageRating ? agency.averageRating * 5 : 0;
 
-      const score = priorityScore + experienceBonus;// + ratingBonus;
+      const score = priorityScore + experienceBonus;
 
       return { agency, score };
     }).sort((a, b) => b.score - a.score);
 
     res.json(matchedAgencies);
+
   } catch (err) {
     console.error('Matching error:', err);
     res.status(500).send('Server error');
